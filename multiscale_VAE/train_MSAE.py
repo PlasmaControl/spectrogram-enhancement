072e4cd0f0ca05f3b382d0cf044cb95148a5cc02
andybroth <ar0535@princeton.edu>
2022-01-25 11:53:34 -0500

    Removed extra file, no more fancy

--- multiscale_VAE/train_MSAE.py
+++ multiscale_VAE/train_MSAE.py
@@ -188,6 +188,41 @@ def get_samples(file, num_samples, spectrograms, final):
     
     return (Sxx_train_reshaped, Sxx_test_reshaped, Sxx_tune_reshaped, final_train_reshaped, final_test, final_tune_reshaped, Sxx_test)
     
+def MSConv2D(initial, nodes, kernels):
+    '''
+    Creates layers for Multi-scale 2D Convolution w/ 3 different kernel sizes
+    '''
+    # Kernel 1
+    x1 = layers.Conv2D(nodes, (kernels[0],kernels[0]), activation="relu", padding="same")(initial)
+    x1 = layers.MaxPooling2D((2, 2), padding="same")(x1)
+    
+    # Kernel 2
+    x2 = layers.Conv2D(nodes, (kernels[1],kernels[1]), activation="relu", padding="same")(initial)
+    x2 = layers.MaxPooling2D((2, 2), padding="same")(x2)
+    
+    # Kernel 3
+    x3 = layers.Conv2D(nodes, (kernels[2],kernels[2]), activation="relu", padding="same")(initial)
+    x3 = layers.MaxPooling2D((2, 2), padding="same")(x3)
+    
+    # Sum together
+    return layers.Add()([x1, x2, x3])
+
+def MSConv2DTranspose(initial, nodes, kernels):
+    '''
+    Creates layers for Multi-scale 2D Deconvolution w/ 3 different kernel sizes 
+    '''
+    # Kernel 1
+    x1 = layers.Conv2DTranspose(nodes, (kernels[0],kernels[0]), strides=2, activation="relu", padding="same")(initial)
+    
+    # Kernel 2
+    x2 = layers.Conv2DTranspose(nodes, (kernels[1],kernels[1]), strides=2, activation="relu", padding="same")(initial)
+    
+    # Kernel 3
+    x3 = layers.Conv2DTranspose(nodes, (kernels[2],kernels[2]), strides=2, activation="relu", padding="same")(initial)
+    
+    # Sum together
+    return layers.Add()([x1, x2, x3])
+
 
 if __name__ == '__main__':
     # Samples (will be 20*num_samples because 20 channels)
