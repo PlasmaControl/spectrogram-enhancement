3967cd418a3800f59647578d81bdb90b5d277836
andybroth <ar0535@princeton.edu>
2022-06-23 17:12:03 -0400

    removed callback problem

--- multiscale_VAE/train_MSAE.py
+++ multiscale_VAE/train_MSAE.py
@@ -478,7 +478,8 @@ if __name__ == '__main__':
                 shuffle=True,
                 validation_data=(Sxx_tune_reshaped, final_tune_reshaped),
                 verbose=2,
-                callbacks=[tensorboard_callback, trainingCallback],
+                # callbacks=[tensorboard_callback, trainingCallback],
+                callbacks=[tensorboard_callback],
             )
         
         ### Make some plots and save errors
