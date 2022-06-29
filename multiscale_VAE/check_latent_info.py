b1bf56f073c90bc14bcfd5de3cfd44942065bc53
andybroth <ar0535@princeton.edu>
2022-06-28 20:29:43 -0400

    fixing

--- multiscale_VAE/check_latent_info.py
+++ multiscale_VAE/check_latent_info.py
@@ -249,9 +249,9 @@ def process_inputs(x, y, window_size, num_strips):
     assert(np.shape(x_valid)[0] == np.shape(y_valid)[0])
     
     # Swap freq and time in x
-    x_test = np.transpose(x_test, axes=[0,1,3,2])
-    x_train = np.transpose(x_train, axes=[0,1,3,2])
-    x_valid = np.transpose(x_valid, axes=[0,1,3,2])
+    x_test = np.transpose(x_test, axes=[0,2,1])
+    x_train = np.transpose(x_train, axes=[0,2,1])
+    x_valid = np.transpose(x_valid, axes=[0,2,1])
     
     # Reshape x to have 1 at end
     x_test = reshape(x_test)
